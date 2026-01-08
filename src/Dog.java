34a16cdf8048015d957842650538aae409120ef5
tompass8 <tom.passerman@ynov.com>
2026-01-08 12:22:23 +0100

    interfaces

--- src/Dog.java
+++ src/Dog.java
@@ -1,21 +1,22 @@
-public class Dog extends Animal {
+public class Dog extends Animal implements Pet {
+
     public Dog (String nom , int age ) {
           super(nom, age);
           this.color= "black";
             setHeight(6);
-
-
     }
 
     @Override
     public void eat() {
         System.out.println("the dog is eating");
-
-
     }
 
     @Override
     public void eat(int amount) {
         System.out.println("the dog is eating"+ amount);
+    }
+
+    public void play() {
+
     }
 }
